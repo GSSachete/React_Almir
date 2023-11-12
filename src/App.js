570cfa964b492708b44bfd1aa92b3083add681f4
JoaoPedroBMoraes <jpbuenomoraes3@gmail.com>
2023-11-12 19:30:38 -0300

    testegit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> fodase.
+          Edit <code>src/App.js</code> aaaaaaaaaaaaa.
         </p>
         <a
           className="App-link"
